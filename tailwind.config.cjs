d6eccd0071e6a5ef183f34166685192a8e1f67fd
Vega <vegalin1029@gmail.com>
2026-03-20 12:41:41 +0800

    fix: add @tailwindcss/typography plugin + remove AdSense placeholders

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,5 +3,5 @@ module.exports = {
   theme: { extend: { colors: { primary: {
     50:'#f0f4f8',100:'#d9e2ec',200:'#bcccdc',300:'#9fb3c8',400:'#829ab1',500:'#627d98',600:'#486581',700:'#334e68',800:'#243b53',900:'#1e3a5f',950:'#102a43',
   }}}},
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
